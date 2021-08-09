3c701a516e89bc091f82b28cdccfaafe85eb52be
Hiep Nguyen <44934122+hiepnguyenit@users.noreply.github.com>
2021-08-09 16:56:07 +0700

    update final project

--- Project/webcam/views.py
+++ Project/webcam/views.py
@@ -1,17 +1,7 @@
-from django.http import HttpResponse
-from django.http.response import StreamingHttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 from django.template import loader
 import time
-from collections import Counter
-import pickle as pkl
-import torch 
-from torch.autograd import Variable
-import cv2 
-from util.parser import load_classes
-from util.model import Darknet
-from util.image_processor import prep_image
-from util.utils import non_max_suppression
-
+from django.views.decorators import gzip
 
 # xac dinh cac url chinh
 def home(request):
@@ -26,119 +16,15 @@ def camera(request):
     template = loader.get_template('camera.html')
     return HttpResponse(template.render({}, request))
 
-def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render({}, request))
-
 
 def stream():
-
-    confidence = 0.5
-    nms_thesh = 0.4
-    start = 0
-    CUDA = torch.cuda.is_available()
-    classes = load_classes("data/coco.names")
-
-    # print("Đang load cấu hình và trọng số mô hình.....")
-    model = Darknet("config/pre-yolov3.cfg")
-    model.load_weights("weights/pre-yolov3.weights")
-    # print("mô hình load thành công!")
-
-    num_classes = model.num_classes
-    inp_dim = 640
-    assert inp_dim % 32 == 0 
-    assert inp_dim > 32
-
-    if CUDA:
-        model.cuda()
-
-    # Đặt mô hình ở chế độ evaluation, 
-    model.eval()
-    obj_counter = {}
-
-    def write(x, results):
-        c1 = tuple(x[1:3].int())
-        c2 = tuple(x[3:5].int())
-        img = results
-        cls = int(x[-1])
-        label = "{0}".format(classes[cls]) ## label = "{0}: {1}".format(classes[cls],str(obj_counter[cls]))
-        color = colors[cls%100]
-        cv2.rectangle(img, c1, c2,color, 1)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-        c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-        cv2.rectangle(img, c1, c2,color, -1)
-        cv2.putText(img, "Empty: {0}".format(str(obj_counter[0])), (5,30), cv2.FONT_HERSHEY_DUPLEX, 1, [96,0,128], 2)
-        cv2.putText(img, "Occupied: {0}".format(str(obj_counter[1])), (5,60), cv2.FONT_HERSHEY_DUPLEX, 1, [96,0,128], 2)
-        cv2.putText(img, "Total: {0}".format(str(output.size(0))), (5,90), cv2.FONT_HERSHEY_DUPLEX, 1, [96,0,128], 2)
-        cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
-        return img
-
-    cap = cv2.VideoCapture("test.mp4")
-
-    # Đặt độ phân giải của frame, nhận các độ phân giải Mặc định
-    # chuyển đổi từ float sang int
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-
-    frames = 0  
-    start = time.time()
-
     while True:
-        ret, frame = cap.read()
-        
-        if ret:   
-            img = prep_image(frame, inp_dim)
-            im_dim = frame.shape[1], frame.shape[0]
-            im_dim = torch.FloatTensor(im_dim).repeat(1,2)   
-                        
-            if CUDA:
-                im_dim = im_dim.cuda()
-                img = img.cuda()
-
-            with torch.no_grad():
-                output = model(Variable(img, volatile = True))
-            output = non_max_suppression(output, confidence, num_classes, nms_conf = nms_thesh)
-
-
-            if type(output) == int:
-                frames += 1
-                print("FPS of the video is {:5.4f}".format( frames / (time.time() - start)))
-                continue
-            
-            
-            obj_counter = output[:,-1]
-            obj_counter = Counter(obj_counter.cpu().numpy().astype(int).tolist())
-
-
-            im_dim = im_dim.repeat(output.size(0), 1)
-            scaling_factor = torch.min(inp_dim/im_dim,1)[0].view(-1,1)
-            
-            output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim[:,0].view(-1,1))/2
-            output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim[:,1].view(-1,1))/2
-            
-            output[:,1:5] /= scaling_factor
-
-            for i in range(output.shape[0]):
-                output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim[i,0])
-                output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim[i,1])
-            
-            classes = load_classes('data/coco.names')
-            colors = pkl.load(open("colors/pallete", "rb"))
-            list(map(lambda x: write(x, frame), output))
-
-            if not cap.isOpened() or not cap.isOpened():
-                break
-            frames += 1
-            print(time.time() - start)
-            print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
-        else:
-            break
-
-        cv2.imwrite('demo.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + open('demo.jpg', 'rb').read() + b'\r\n')
+        time.sleep(1)
 
+
+@gzip.gzip_page
 def video_feed(request):
     return StreamingHttpResponse(stream(), content_type='multipart/x-mixed-replace; boundary=frame')
-
     
